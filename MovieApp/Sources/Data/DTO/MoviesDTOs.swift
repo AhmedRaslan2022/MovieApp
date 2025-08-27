9a5eadf3ad296fd3559c1845ffcc6a6422974ce0
Ahmed Raslan <a.rasslan01@gmail.com>
2025-08-27 14:05:27 +0300

    Merge dev into main (#8)
    * refactor: isolate core data logger in a new file and renamed to proper name
    
    * refactor: change dto fields to optional to avoid decoding issues

--- MovieApp/Sources/Data/DTO/MoviesDTOs.swift
+++ MovieApp/Sources/Data/DTO/MoviesDTOs.swift
@@ -25,16 +25,16 @@ struct MovieDTO: Codable {
     let backdropPath: String?
     let genreIds: [Int]
     let id: Int
-    let originalLanguage: String
-    let originalTitle: String
-    let overview: String
-    let popularity: Double
-    let posterPath: String
-    let releaseDate: String
-    let title: String
-    let video: Bool
-    let voteAverage: Double
-    let voteCount: Int
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
     
  
     enum CodingKeys: String, CodingKey {
